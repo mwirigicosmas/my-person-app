fc93433dc7c4971a4e657ae517d760fabaa69c61
CosiiRiggz <mwirigicosmas@live.com>
2018-11-28 17:45:14 +0300

    REACT BASICS

--- src/App.js
+++ src/App.js
@@ -1,27 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons:[
+      {name:'Cosmas', age: 24},
+      {name:'Mwirigi', age:28},
+      {name:'Mutethia', age: 30}
+    ],
+    otherValue: 'my names'
+
+  }
+
+  switchNameHandler = (newName) => {
+    console.log(typeof this.switchNameHandler);
+    // DO NOT CHANGE / MUTATE STATE DIRECTLY : this.state.persons[0].name = 'cosiiriggz';
+    this.setState({
+      persons:[
+        {name:newName, age: 30},
+        {name:'Riggz', age:15},
+        {name:'COSIIRIGGZ', age: 24}
+      ]
+    });
+  }
+
+  nameChangedHandler = (event) =>{
+    this.setState({
+      persons:[
+        {name:'Cosmas', age: 30},
+        {name:event.target.value, age:15},
+        {name:'COSIIRIGGZ', age: 24}
+      ]
+    });
+
+  }
   render() {
+    // =========================================================================
+    // INLINE STYLING IN JAVASCRIPT
+    // =========================================================================
+
+    const style = {
+      backgroundColor: 'blue',
+      font:'inherit',
+      border: '1px solid black',
+      padding: '8px',
+      color: 'white',
+      cursor: 'pointer'
+
+    }
+    // =========================================================================
+    // This is javascript XML JSX not html.
+    // =========================================================================
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi am cosmas mwirigi a react developer</h1>
+        <p>javascript is the language of the future</p>
+        <button style = {style} onClick = {this.switchNameHandler.bind(this, 'MWIRIGI COSMAS')}>Switch Names</button>
+        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
+        <Person name = {this.state.persons[1].name} age={this.state.persons[1].age} 
+         click = {this.switchNameHandler.bind(this,'yeah!!')} 
+         changed = {this.nameChangedHandler}>My hobbies: react apps
+        </Person>
+        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
       </div>
-    );
+     );
+     
+    // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Hi, I\'m a react app')   ); 
+    
   }
 }
 
